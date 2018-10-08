b00945cd41370c2866f83695a58954219e2b0c8f
PeterDavidMulligan <mcgrathjohnjerome@gmail.com>
2018-10-08 18:50:05 +0100

    Finished employee/company edit view

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -6,12 +6,14 @@
       <div class="col-md-12">
         <div class="card">
           <div class="card-header">Edit Company</div>
+
           <div class="card-body">
               @if (session('status'))
                   <div class="alert alert-success" role="alert">
                       {{ session('status') }}
                   </div>
               @endif
+
               <table class="table">
                 <thead>
                   <tr>
@@ -21,8 +23,30 @@
                     <th>Website</th>
                   </tr>
                 </thead>
+
                 <tbody>
+                  <tr>
+                    <td>{{$company->name}}</td>
+                    <td>{{$company->email}}</td>
+                    <td>{{ Html::image($company->logo) }}</td>
+                    <td>{{$company->website}}</td>
+                  </tr>
+
+                  <tr>
+                    <td><input type="text" class="form-control" name="name" value="{{$company->name}}"></td>
+                    <td><input type="text" class="form-control" name="name" value="{{$company->email}}"></td>
+                    <td><input type="file"/></td>
+                    <td><input type="text" class="form-control" name="name" value="{{$company->website}}"></td>
+                  </tr>
                 </tbody>
+              </table>
+              <div class="row justify-content-center">
+                <form action="{{action('CompanyController@edit', $company->id)}}" method="post">
+                  @csrf
+                  <input name="_method" type="hidden" value="put">
+                  <button class="btn btn-warning" type="submit">Edit</button>
+                </form>
+              </div>
         </div>
       </div>
     </div>
--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -23,7 +23,30 @@
                   </tr>
                 </thead>
                 <tbody>
+                  <tr>
+                    <td>{{$employee->first_name}}</td>
+                    <td>{{$employee->last_name}}</td>
+                    <td>{{$employee->company}}</td>
+                    <td>{{$employee->email}}</td>
+                    <td>{{$employee->phone}}</td>
+                  </tr>
+
+                  <tr>
+                    <td><input type="text" class="form-control" value="{{$employee->first_name}}"></td>
+                    <td><input type="text" class="form-control" value="{{$employee->last_name}}"></td>
+                    <td><input type="number" step="1" min="0" value="{{$employee->company}}"></td>
+                    <td><input type="text" class="form-control" value="{{$employee->email}}"></td>
+                    <td><input type="text" class="form-control" value="{{$employee->phone}}"></td>
+                  </tr>
                 </tbody>
+              </table>
+              <div class="row justify-content-center">
+                <form action="{{action('EmployeeController@edit', $employee->id)}}" method="post">
+                  @csrf
+                  <input name="_method" type="hidden" value="put">
+                  <button class="btn btn-warning" type="submit">Edit</button>
+                </form>
+              </div>
         </div>
       </div>
     </div>
