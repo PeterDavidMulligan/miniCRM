49e7744305939e785f39e072a951baf9544e00c8
PeterDavidMulligan <mcgrathjohnjerome@gmail.com>
2018-10-09 18:34:30 +0100

    removed table from company edit view that was stopping the form from firing

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -4,10 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-12">
+
         <div class="card">
           <div class="card-header">Edit Company</div>
-
           <div class="card-body">
+
               @if (session('status'))
                   <div class="alert alert-success" role="alert">
                       {{ session('status') }}
@@ -28,28 +29,35 @@
                   <tr>
                     <td>{{$company->name}}</td>
                     <td>{{$company->email}}</td>
-                    <td>{{ Html::image($company->logo) }}</td>
-                    <td>{{$company->website}}</td>
-                  </tr>
 
-                  <tr>
-                    <td><input type="text" class="form-control" name="name" value="{{$company->name}}"></td>
-                    <td><input type="text" class="form-control" name="name" value="{{$company->email}}"></td>
-                    <td><input type="file"/></td>
-                    <td><input type="text" class="form-control" name="name" value="{{$company->website}}"></td>
+                    <!-- Hack to make seeded image urls work -->
+                    <?php if (strpos($company->logo, 'lorem') !== false): ?>
+                      <td>{{ Html::image($company->logo) }}</td>
+                    <?php else: ?>
+                      <td>{{ Html::image( URL::to('/') . '/logos/' . $company->logo) }}</td>
+                    <?php endif; ?>
+
+                    <td>{{$company->website}}</td>
                   </tr>
                 </tbody>
               </table>
-              <div class="row justify-content-center">
-                <form action="{{action('CompanyController@edit', $company->id)}}" method="post">
-                  @csrf
-                  <input name="_method" type="hidden" value="put">
-                  <button class="btn btn-warning" type="submit">Edit</button>
+              <div class="row">
+                <form enctype="multipart/form-data" name ="editCompanyForm" action="{{action('CompanyController@edit', $company->id)}}" method="post">
+                    <input type="text" class="form-control col-lg-2" name="name" value="{{$company->name}}">
+                    <input type="text" class="form-control col-lg-2" name="email" value="{{$company->email}}">
+                    <input type="file" class="form-control col-lg-2" name="logo"/>
+                    <input type="text" class="form-control col-lg-6" name="website" value="{{$company->website}}">
+                    <br><br>
+                    @csrf
+                    <input name="_method" type="hidden" value="put">
+                    <button class="btn btn-warning" type="submit">Edit</button>
                 </form>
               </div>
         </div>
       </div>
     </div>
+
   </div>
+</div>
 </div>
 @endsection
