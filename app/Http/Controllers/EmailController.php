368f6bcd1424ee60172685b66b8f6d51d87906bc
PeterDavidMulligan <aezur20@gmail.com>
2018-10-11 08:26:03 +0100

    Added EmailController and NewCompany mail

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace miniCRM\Http\Controllers;
+
+use Illuminate\Http\Request;
+use miniCRM\Mail\NewCompany;
+
+class EmailController extends Controller
+{
+    public function sendMail()
+    {
+      $user = Auth::user();
+      \Mail::to($user)->send(new NewCompany);
+      return redirect()->home();
+    }
+}
